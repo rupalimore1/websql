f2d201a5642b9ec85d78778c9e46e6512ec92fa3
rupalimore1 <48875932+rupalimore1@users.noreply.github.com>
2020-01-19 14:34:38 +0530

    Delete KeyVault.ps1

--- KeyVault.ps1
+++
@@ -1,4 +0,0 @@
-Connect-AzAccount
-Select-AzSubscription -SubscriptionId '202e1a05-a019-458e-9619-3d0325f48f8e'
-New-AzResourceGroup -Name 'AzureDevOpstraing1' -Location 'centralus' 
-New-AzResourceGroupDeployment -ResourceGroupName 'AzureDevOpstraing1' -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json 
